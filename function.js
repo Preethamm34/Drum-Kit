4badd00d46b448b31c4788f46262fbd7357098c0
Preethamm34 <118427179+Preethamm34@users.noreply.github.com>
2022-11-25 13:14:59 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,27 @@
+function playW(){
+ document.getElementById('w').play();
+}
+
+function playA(){
+ document.getElementById('a').play();
+}
+
+function playS(){
+ document.getElementById('s').play();
+}
+
+function playD(){
+ document.getElementById('d').play();
+}
+
+function playJ(){
+ document.getElementById('j').play();
+}
+
+function playK(){
+ document.getElementById('k').play();
+}
+
+function playL(){
+ document.getElementById('l').play();
+}
